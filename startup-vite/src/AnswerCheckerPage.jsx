867e52fdc5d26d17a738e6ce8be1d2ed1459653f
TagNakamoto <tahoshna@byu.edu>
2024-11-08 18:51:14 -0700

    Altered CSS to my liking

--- startup-vite/src/AnswerCheckerPage.jsx
+++ startup-vite/src/AnswerCheckerPage.jsx
@@ -8,27 +8,29 @@ function AnswerChecker() {
     const navigate = useNavigate();
     const [answer, setAnswer] = useState('');
     const [message, setMessage] = useState('');
+
     const handleLogoutClick = (e) => {
         e.preventDefault();
         navigate('/');
     };
     function handleCheck(e) {
-        e.preventDefault;
-        const answer = document.getElementById("answer");
+        e.preventDefault();
+        setMessage(`Answer checked: ${answer}`);
 
     }
 
     return (
         <div>
             <h2>Input your calculated value for the homework</ h2>
-            <form method="get" action="#">
+            <form onSubmit={ handleCheck }>
                 <div>
                     <span>Question 1</span>
-                    <input type="number" id="answer" placeholder="your answer" />
+                    <input type="number" id="answer" value={answer} placeholder="your answer" onChange={(e) => setAnswer(e.target.value)} />
                 </div>
                     <button type="submit">Check</button>
             </form>
-
+            { }
+            {message && <p>{message}</p>}
             <form method="get" action="#">
                 <div>
                     <button type="button" onClick={handleLogoutClick}>Log Out</button>
--- startup-vite/src/App.jsx
+++ startup-vite/src/App.jsx
@@ -14,34 +14,38 @@ import AnswerStats from "./AnswerStatistics"
 
 const App = () => (
     <BrowserRouter>
-        <header>
-            <h1>
-                Answer Checker <img alt="Checkmark" width="30" height="30" src="https://www.publicdomainpictures.net/pictures/130000/velka/check-mark-icon.jpg" />
-            </h1>
-            <div className='app'>
-                <NavLink to='/'>Login</NavLink>
-                <NavLink to='/AnswerCheckerPage'>Answer Checker</NavLink>
-                <NavLink to='/AnswerStatistics'>Answer Statistics</NavLink>
-            </div>
-        </header>
+        <body>
+            <header>
+                <h1>
+                    Answer Checker <img alt="Checkmark" width="30" height="30" src="https://www.publicdomainpictures.net/pictures/130000/velka/check-mark-icon.jpg" />
+                </h1>
+                <div className='app'>
+                    <NavLink to='/'>Login</NavLink>
+                    <NavLink to='/AnswerCheckerPage'>Answer Checker</NavLink>
+                    <NavLink to='/AnswerStatistics'>Answer Statistics</NavLink>
+                </div>
+            </header>
 
-        <main>
-            <Routes>
-                <Route path='/' element={<Login />}/>
-                <Route path='/AnswerCheckerPage' element={<AnswerChecker />}/>
-                <Route path='/AnswerStatistics' element={<AnswerStats />} />
-                <Route path='*' element={<Navigate to='/' replace/>} />
-            </Routes>
-        </main>
+            <main>
+                <Routes>
+                    <Route path='/' element={<Login />} />
+                    <Route path='/AnswerCheckerPage' element={<AnswerChecker />} />
+                    <Route path='/AnswerStatistics' element={<AnswerStats />} />
+                    <Route path='*' element={<Navigate to='/' replace />} />
+                </Routes>
+            </main>
 
 
-        <footer>
-            <div className="container-fluid">
-                <span className="text-reset">Author:Taggart Nakamoto</span>
-                <a href="https://github.com/TagNakamoto/startup/tree/main">Project GitHub</a>
-            </div>
+            <footer>
+                <div className="container-fluid">
+                    <span className="text-reset">Author:Taggart Nakamoto</span>
+                    <a href="https://github.com/TagNakamoto/startup/tree/main">Project GitHub</a>
+                </div>
 
-        </footer>
+            </footer>
+
+        </body>
+        
     </BrowserRouter>
 );
 
