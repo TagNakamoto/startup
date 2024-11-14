236c83ee580325e57ac02d07dac00836fa2a8ba2
TagNakamoto <tahoshna@byu.edu>
2024-11-13 20:04:40 -0700

    Tried implementing login and user creation

--- startup-vite/src/index.jsx
+++ startup-vite/src/index.jsx
@@ -4,16 +4,58 @@ import '../../indexcss.css';
 
 function Login() {
     const navigate = useNavigate();
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [displayError, setDisplayError] = React.useState(null);
 
-    const handleLoginClick = (e) => {
+    const handleLoginClick = async (e) => {
         e.preventDefault();
-        navigate('/AnswerCheckerPage');
+        try {
+            const response = await fetch('/api/auth/login', {
+                method: "post",
+                body: JSON.stringify({ username, password }),
+                headers: {
+                    "Content-Type": "application/json; charset=UTF-8",
+                },
+            });
+            if (response.ok) {
+                const token = await response.json();
+                navigate('/AnswerCheckerPage');
+            }
+            else {
+                const errorData = await response.json();
+                setDisplayError(errorData.msg);
+            }
+        }
+        catch (err) {
+            setDisplayError("There was an error in your login");
+        }
+        
     };
 
 
-    const handleCreateClick = (e) => {
+    const handleCreateClick = async (e) => {
         e.preventDefault();
-        navigate('/AnswerCheckerPage');
+        try {
+            const response = await fetch('/api/auth/create', {
+                method: "post",
+                body: JSON.stringify({ username, password }),
+                headers: {
+                    "Content-Type": "application/json; charset=UTF-8",
+                },
+            });
+            if (response.ok) {
+                const token = await response.json();
+                navigate('/AnswerCheckerPage');
+            }
+            else {
+                const errorData = await response.json();
+                setDisplayError(errorData.msg);
+            }
+        }
+        catch (err) {
+            setDisplayError("There was an error in your login");
+        }
     };
 
 
@@ -23,11 +65,11 @@ function Login() {
             <form method="get" action="#">
                 <div>
                     <span>Username</span>
-                    <input type="text" placeholder="your username" />
+                    <input type="text" placeholder="your username" value={username} onChange={(e) =>setUsername(e.target.value) } />
                 </div>
                 <div>
                     <span>Password</span>
-                    <input type="password" placeholder="password" />
+                    <input type="password" placeholder="password" value={password} onChange={(e)=> setPassword(e.target.value) } />
                 </div>
                 <button type="button" onClick={handleLoginClick}>Login</button>
                 <button type="button" onClick={handleCreateClick}>Create</button>
