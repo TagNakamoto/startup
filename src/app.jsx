5957622a11820232b78d38ab22538140d9b075b6
TagNakamoto <tahoshna@byu.edu>
2025-02-08 18:11:21 -0700

    Implemented what I learned from react p1

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+
+export default function App() {
+    return (
+        <div className='body bg-dark text-light'>
+            <header className='container-fluid'>
+            </header>
+
+            <main>App components go here</main>
+
+            <footer className='bg-dark text-white-50'>
+                <div className='container-fluid'>
+                    <span className='text-reset'>Author Name(s)</span>
+                    <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+                        Source
+                    </a>
+                </div>
+            </footer>
+        </div>
+    );
+}
