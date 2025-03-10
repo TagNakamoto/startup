9b3609b075ab291447be453f77f4185df60f5693
TagNakamoto <tahoshna@byu.edu>
2025-03-09 20:33:02 -0600

    Added post and put for logging in and creating user. Tested it using curl

--- service/index.js
+++ service/index.js
@@ -2,7 +2,6 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
-const e = require('express');
 const app = express();
 
 const authCookieName = 'token';
@@ -11,12 +10,66 @@ let users = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000
 
-app.use(express.json);
+app.use(express.json());
 
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
 //Router
 var apiRouter = express.Router();
 app.use('/api', apiRouter);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+function setAuthCookie(res, user) {
+    user.token = uuid.v4();
+
+    res.cookie(authCookieName, user.token, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = {
+        email: email,
+        password: passwordHash,
+    };
+    users.push(user);
+
+    return user;
+}
+
+function getUser(field, value) {
+    if (value) {
+        return users.find((user) => user[field] === value);
+    }
+    return null;
+}
+
+//Create User
+app.post('/api/auth', async (req, res) => {
+    if (await getUser('email', req.body.email)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.email, req.body.password);
+        setAuthCookie(res, user);
+        res.send({ email: user.email });
+    }
+})
+
+//Log in User
+app.put('/api/auth', async (req, res) => {
+    const user = await getUser('email', req.body.email);
+    if (user && (await bcrypt.compare(req.body.password, user.password))) {
+        setAuthCookie(res, user);
+        res.send({ email: user.email });
+    }
+    else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+})
